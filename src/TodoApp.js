78f20d82aca8ef1247cfce4fe870dec03c3078b9
jjgouveia <gouvik.dev@gmail.com>
2022-09-05 14:12:39 -0300

    versão 0.03

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -4,30 +4,54 @@ import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Footer from "./components/Footer";
+import { nanoid } from "nanoid";
 
 export default function TodoApp() {
   const [task, setTask] = useState("");
   const [itemsList, setItemsList] = useState([]);
 
-  const handleInput = ({ target: { value, type, checked, id } }) => {
+  const handleInput = ({ target: { value, type, checked, id: ide } }) => {
     if (type === "text") {
       const inputTask = value;
       setTask(inputTask);
       return;
     }
     if (checked) {
-      document.getElementById(id).className = "checked";
+      document.querySelectorAll(`#${ide}`)[0].className="checked"
     } else {
-      document.getElementById(id).className = "";
+      document.querySelectorAll(`#${ide}`)[0].className=""
     }
   };
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = itemsList.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTask(updatedTasks);
+  }
+
+  const deleteTask = (id) => {
+    console.log(id);
+  }
 
   const handleList = (e) => {
     e.preventDefault();
     if (!task) return;
-    setItemsList([...itemsList, task]);
+    const newTask = { id: `todo-${nanoid()}`, task, completed: false }
+    setItemsList([...itemsList, newTask]);
+
     setTask("");
   };
+
+  const plural = itemsList.length !== 1 ? 's' : '';
+  const remainingTasks = `${itemsList.length} tarefa${plural} restante${plural}`
+  console.log(remainingTasks);
 
   return (
     <div className="todoapp stack-large">
@@ -38,20 +62,23 @@ export default function TodoApp() {
       handleInput={ handleInput }
       />
       <FilterButton />
+      <h3>{ itemsList.length !== 0 ? remainingTasks : 'A lista ainda está vazia!' }</h3>
       <ul
         area-aria-labelledby="list-heading"
         className="todo-list stack-large stack-exception"
       >
-        {itemsList.length ? (
+        {
           itemsList?.map((task, index) => (
             <Todo
+            key={ task.id }
             task={ task }
-            index={ index }
-            handleInput={ handleInput }/>
+            handleInput={ handleInput }
+            completed={ task.completed }
+            toggleTaskCompleted={toggleTaskCompleted}
+            deleteTask={ deleteTask }
+            />
           ))
-        ) : (
-          <h3>A lista ainda está vazia!</h3>
-        )}
+        }
       </ul>
       <Footer />
     </div>
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 
-export default function Todo({ task, index, handleInput }) {
-  return (
-    <li key={`task-${index}`} className="">
+export default function Todo({ task, toggleTaskCompleted, completed, deleteTask }) {
+    return (
+        <li className="">
       <div className="">
-        <span id={`task-${index}`} className="todo-label">
-          {task}
+        <span id={task.id} className="todo-label">
+          {task.task}
         </span>
         &nbsp;
-        <input type="checkbox" id={`task-${index}`} onChange={handleInput} />
+        <input type="checkbox" id={task.id} onChange={() => toggleTaskCompleted(task.id)} defaultChecked={ task.completed } value={ completed }/>
         <div className="btn-group">
           <button type="button" className="btn">
             Editar
           </button>
-          <button type="button" className="btn btn__danger">
+          <button type="button" className="btn btn__danger" onClick={ () => deleteTask(task.id) }>
             Deletar
           </button>
         </div>
