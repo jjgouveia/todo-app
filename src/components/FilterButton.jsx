d023d32457619f402b45d69208503b0ab8687ce9
jjgouveia <gouvik.dev@gmail.com>
2022-09-11 14:28:26 -0300

    Implementação dos filtros

--- src/components/FilterButton.jsx
+++ src/components/FilterButton.jsx
@@ -1,22 +1,27 @@
 import React from "react";
 
-export default function FilterButton() {
+export default function FilterButton({ name, setFilter, isPressed }) {
   return (
-    <div className="filters btn-group stack-exception">
-      <button type="button" className="btn toggle-btn" aria-pressed="true">
-        <span className="visually-hidden">Mostrar </span>
-        <span>Todas</span>
-        <span className="visually-hidden"> as Tarefas</span>
-      </button>
-      <button type="button" className="btn toggle-btn" aria-pressed="false">
-        <span className="visually-hidden">Mostrar </span>
-        <span>em Andamento</span>
-      </button>
-      <button type="button" className="btn toggle-btn" aria-pressed="false">
-        <span className="visually-hidden">Mostrar </span>
-        <span className="visually-hidden"> tarefas</span>
-        <span> Finalizadas</span>
-      </button>
-    </div>
+    <button
+      type="button"
+      className="btn toggle-btn"
+      aria-pressed={isPressed}
+      onClick={() => setFilter(name)}
+    >
+      {name === "Todas" ? (
+        <>
+          <span className="visually-hidden">Mostrar </span>
+          <span>{name}</span>
+          <span className="visually-hidden"> as Tarefas </span>
+        </>
+      ) : (
+        <>
+          <span className="visually-hidden">Mostrar</span>
+          <span className="visually-hidden"> as Tarefas </span>
+          <span>{name}</span>
+        </>
+      )}
+    </button>
+    //o visually-hidden precisa ser condicional para o "todas"
   );
 }
